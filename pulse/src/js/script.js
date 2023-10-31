0881a9ecd6203d79de7eeb027c54f5d7ff653587
Javokh <javoh1104@gmail.com>
2023-10-31 17:09:07 +0100

    animate.css

--- pulse/src/js/script.js
+++ pulse/src/js/script.js
@@ -109,5 +109,22 @@ $(document).ready(function () {
         $('form').trigger('reset');
     });
     return false;
-});
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+  
+  new WOW().init();
+  
 });
